4608edaf6cd435b7eb4c49949fac8cea4e824ce2
Kai Liang Cheng <tonyischinese@gmail.com>
2015-12-06 18:20:24 -0500

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,42 @@
+//Author: Kai Cheng
+
+package kaleidoscope;
+
+public class Rectangle {
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	
+	public Rectangle(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+}
